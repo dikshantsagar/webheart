86dd2987714ab25d2881490a54b96e1f8ba38cd6
Dikshant Sagar <sagar.dikshant@gmail.com>
2022-02-13 12:04:39 +0530

    Update settings.py

--- webheart/settings.py
+++ webheart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6c$73wz2dw7v1fr3a2gj&t7#xnerxr9@)kiwlt8n0ncmb_kv4#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
